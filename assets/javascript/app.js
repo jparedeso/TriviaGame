62b9226032f16d278bd4980d9345c7c472154c9e
jparedeso <jparedeso@gmail.com>
2018-01-21 15:25:29 -0500

    added array of questions with object containing choices and correct answer.

---
+++ assets/javascript/app.js
@@ -0,0 +1,81 @@
+$(function() {
+
+var correctCount = 0;
+var incorrectCount = 0;
+var unansweredCount = 0;
+
+var questions = [
+	{
+		"question": "Which album features the song 'Come Together'?",
+		"choices" : ["Help!", "Rubber Soul", "Revolver", "Abbey Road"],
+		"answer" : 3
+	},
+	{
+		"question": "Which of these songs was written by the band Aerosmith?",
+		"choices" : ["Detroit Rock City", "Runnin' With the Devil", "Whole Lotta Love", "Sweet Emotion"],
+		"answer" : 3
+	},
+	{
+		"question": "What album is certified as the best-selling of all time with 46 million sales?",
+		"choices" : ["Hotel California", "Thriller", "Rummours", "Back in Black"],
+		"answer" : 1
+	},
+	{
+		"question": "What song features the lyrics 'we don't need no education'?",
+		"choices" : ["Hot for Teacher", "Another Brick in the Wall", "Don't Stand So Close to Me", "School's Out"],
+		"answer" : 1
+	},
+	{
+		"question": "Which of these bands did not originate from the United Kingdom?",
+		"choices" : ["Pink Floyd", "Black Sabbath", "The Doors", "Queen"],
+		"answer" : 2
+	},
+	{
+		"question": "Which modern pop artist is known for singing 'Firework'?",
+		"choices" : ["Adele", "Nicki Minaj", "Katy Perry", "Taylor Swift"],
+		"answer" : 2
+	},
+	{
+		"question": "Who won the 2015 Grammy Award for Record of the Year?",
+		"choices" :["Iggy Azalea", "Meghan Trainor", "Sia", "Sam Smith"],
+		"answer" : 3
+	},
+	{
+		"question": "Which of these songs does not begin with a drum solo?",
+		"choices" : ["Jump - Van Halen", "Billie Jean - Michael Jackson", "Walk This Way - Aerosmith", "My Sharona - The Knack"],
+		"answer" : 0
+	},
+	{
+		"question": "Which of these songs does not begin with a drum solo?",
+		"choices" : ["Drums", "Guitar", "Keyboards", "Bass"],
+		"answer" : 1
+	},
+	{
+		"question": "Which '90s rapper performed the song 'Gangsta's Paradise'?",
+		"choices" : ["Notorious B.I.G.", "Ice Cube", "Snoop Dog", "Coolio"],
+		"answer" : 3
+	}
+];
+
+$("#myStart").click(function() {
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
